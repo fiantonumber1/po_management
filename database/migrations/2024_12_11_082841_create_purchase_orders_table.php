ba958d93a7d058e10b9917ccff38ab6fd6bb372b
fiantonumber1 <87689030+fiantonumber1@users.noreply.github.com>
2025-02-11 14:12:26 +0700

    update

---
+++ database/migrations/2024_12_11_082841_create_purchase_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('po_number')->unique();
+            $table->date('po_date');
+            $table->string('invoice_number')->unique();
+            $table->date('invoice_date');
+            $table->date('due_date');
+            $table->string('buyer');
+            $table->decimal('grand_total', 15, 2);
+            $table->enum('status', ['Pending', 'PAID'])->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_orders');
+    }
+};
